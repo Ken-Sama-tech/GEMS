2ee099dd86d37b5efaa99a267c373600ea9b0bba
Ken-Sama-tech <dikoalambastaacc@gmail.com>
2025-01-07 17:15:13 +0800

    woah
    idk

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //show only articlesections of selected article 
 
-    const showArticleSectionsOfSelectedArticle = {
-        const article = document.querySelector('#article');
-        articleSection: function () {
-
-        }
-    }
 });
